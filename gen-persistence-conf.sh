55b1dd933c4504f9ce7b1a93c0e5846d2cbf7e1f
Donald Raikes <don.raikes@gmail.com>
2013-06-12 11:49:28 -0700

    Added kali repositories to the livecd.

---
+++ gen-persistence-conf.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+echo "/etc" >> /mnt/persistence.conf
+echo "/git" >> /mnt/persistence.conf
+echo "/home" >> /mnt/persistence.conf
+echo "/lib union" >> /mnt/persistence.conf
+echo "/root" >> /mnt/persistence.conf
+echo "/usr union" >> /mnt/persistence.conf
+echo "/var union" >> /mnt/persistence.conf
